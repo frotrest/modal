cc6213ca1cc2f5f24e83cca9f1ee51bdd6a9721c
Frotrest <125584696+frotrest@users.noreply.github.com>
2026-02-14 22:28:51 +0200

    update

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,6 +3,19 @@ import clsx from 'clsx';
 import styles from './modal.module.css'
 
 export default class Modal extends Component {
+    componentDidMount() {
+        document.addEventListener("keydown", this.handleKey);
+        document.body.style.overflow = "hidden";
+    }
+    componentWillUnmount() {
+        document.removeEventListener("keydown", this.handleKey);
+        document.body.style.overflow = "auto";
+    }
+    handleKey = (e) => {
+        if(e.key === "Escape") {
+            this.props.onClose();
+        }
+    }
     render () {
         if(!this.props.show) return null;
         return (
